ff72bc2bb3b992a38411f62f88bc4b9ddaac2271
PaoloL 2T <paololuca@gmail.com>
2022-11-28 15:44:37 +0100

    update ranking post assoluti

---
+++ WpfApp1/DB/Scripts/Update Ranking.sql
@@ -0,0 +1,12 @@
+﻿--update in tabella ranking per disciplina ed atleta
+
+update ranking set
+Punteggio = @punteggio,
+LastUdate = GETDATE(),
+Posizionamento = @posizionamento
+where IdAtleta = @atleta and IdDisciplina = @iddisciplina
+
+
+--insert into storico con nuova fase
+insert into RankingStorico 
+select IdAtleta, Punteggio, GETDATE(), GETDATE(), Posizionamento, IdDisciplina, 2022, @newPhase from ranking

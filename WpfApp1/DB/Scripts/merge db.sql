e10171a0af65e9c32a5bc0241f18f0bc3ff1daa2
PaoloLucaTwoTeam <paololuca@gmail.com>
2024-06-13 11:27:43 +0200

    riconciliazione post assoluti

--- WpfApp1/DB/Scripts/merge db.sql
+++ WpfApp1/DB/Scripts/merge db.sql
@@ -1,7 +1,7 @@
 /*
 select * from [C:\Users\PaoloLuca\Documents\source\GitHub\CGS_Material_2020\WpfApp1\DB\TORNEIFEMMINILE.MDF].dbo.GironiIncontri where IdTorneo  = 29
 select * from [C:\Users\PaoloLuca\Documents\source\GitHub\CGS_Material_2020\WpfApp1\DB\TORNEI.MDF].dbo.GironiIncontri where IdTorneo  = 29
-select * from [C:\Users\PaoloLuca\Documents\source\GitHub\CGS_Material_2020\WpfApp1\DB\TORNEIFEMMINILE.MDF].dbo.Qualificati16  where IdTorneo  = 29
+select * from [C:\Users\PaoloLuca\Documents\source\GitHub\CGS_Material_2020\WpfApp1\DB\TORNEIFEMMINILE.MDF].dbo.Finali  where IdTorneo  = 29
 select * from [C:\Users\PaoloLuca\Documents\source\GitHub\CGS_Material_2020\WpfApp1\DB\TORNEI.MDF].dbo.Finali where IdTorneo  = 29
 */
 

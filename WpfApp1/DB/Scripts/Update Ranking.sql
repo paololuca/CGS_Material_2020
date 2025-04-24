3b86a0ea8ae37887660d6cbb0349445e5e22685c
PaoloLucaTwoTeam <paololuca@gmail.com>
2025-04-24 11:13:01 +0200

    update DB post qualificazione e pre chianciano

--- WpfApp1/DB/Scripts/Update Ranking.sql
+++ WpfApp1/DB/Scripts/Update Ranking.sql
@@ -7,8 +7,12 @@ update ranking set Punteggio = @punteggio, LastUdate = GETDATE(), Posizionamento
 insert into RankingStorico 
 select IdAtleta, Punteggio, GETDATE(), GETDATE(), Posizionamento, IdDisciplina, 2023, @newPhase from ranking
 
-insert
+
 
 --vanno azzerati, se sto facendo l'update del reanking per gli assoluti, tutti quelli ceh non sono stati aggiornati
+-- --> SECONDO ME NON FUNZIONA, NON SO COSA MI ERO FUMATO
 --update RankingStorico set Punteggio = 0, LastUdate = GETDATE(), Posizionamento = 999 where LastUdate < convert(datetime, 11/05/2023 ) and Anno = 2023 and Fase = 10
 
+-- --> QUESTO HA PIù SENSO
+--update Ranking set Posizionamento = 999 where Posizionamento = 0
+--update RankingStorico set Posizionamento = 999 where Posizionamento = 0
